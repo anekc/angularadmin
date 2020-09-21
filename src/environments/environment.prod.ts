db4a57547a6fa5518de2ffe77b15077609c7405a
Alejandro Arteaga <alejow18@gmail.com>
2020-09-21 18:57:16 -0500

    environment listo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@
 // TODO: cambiar el url de los servicios
 export const environment = {
   production: true,
-  base_url: 'http://localhost:3005/api'
+  base_url: 'https://adminprobackend1.herokuapp.com/api'
 };
